7d1f603c55f75c515d462fcd52713fc8d29e66d7
manunasci <nascimentomanuella73@gmail.com>
2021-11-05 22:24:15 -0300

    esse comite adiciona o programa para exibir o número maior

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maior 2 Numeros");
+
+            Console.WriteLine("Digite o primeiro número");
+            string numero = Console.ReadLine();
+            double numero1 = double.Parse(numero);
+
+            Console.WriteLine("Digite o sengudo número");
+            string Numero2 = Console.ReadLine();
+            double numero2 = double.Parse(Numero2);
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("O maior número é " + numero1);
+            }
+
+            else
+            {
+                Console.WriteLine("O maior número é " + numero2);
+            }
+        }
+    }
+}
